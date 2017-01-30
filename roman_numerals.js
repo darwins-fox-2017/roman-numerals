a1146eed039b8192ad6ffb25b3fef2dab3c638de
Adam Saparudin <adamsaparudin@gmail.com>
2017-01-30 11:43:32 +0700

    done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,12 +1,98 @@
 function to_roman_old (n) {
   // your code here
+  let a = '';
+  while(n > 0) {
+    if(n / 1000 > 1) {
+      a += 'M';
+      n -= 1000;
+    }
+    else if (n / 500 > 1) {
+      if (n >= 500)
+      a += 'D';
+      n -= 500;
+    }
+    else if (n / 400 > 1) {
+      a += 'CD';
+      n -= 400;
+    }
+    else if (n / 100 > 1) {
+      a += 'C';
+      n -= 100;
+    }
+    else if (n / 50 > 1) {
+      a += 'L';
+      n -= 50;
+    }
+    else if (n / 40 > 1) {
+      a += 'XL';
+      n -= 40;
+    }
+    else if (n / 10 > 1) {
+      a += 'X';
+      n -= 10;
+    }
+    else if (n / 5 > 1) {
+      a += 'V';
+      n -= 5;
+    }
+    else if (n >= 1) {
+      a += 'I';
+      n -= 1;
+    }
+  }
+  return a
 }
 
 function to_roman (n) {
   // your implementation code here
+  let a = '';
+  while(n > 0) {
+    if(n / 1000 > 1) {
+      a += 'M';
+      n -= 1000;
+    }
+    else if (n / 500 > 1) {
+      if (n >= 500)
+      a += 'D';
+      n -= 500;
+    }
+    else if (n / 400 > 1) {
+      a += 'CD';
+      n -= 400;
+    }
+    else if (n / 100 > 1) {
+      a += 'C';
+      n -= 100;
+    }
+    else if (n / 50 > 1) {
+      a += 'L';
+      n -= 50;
+    }
+    else if (n / 40 > 1) {
+      a += 'XL';
+      n -= 40;
+    }
+    else if (n / 10 > 1) {
+      a += 'X';
+      n -= 10;
+    }
+    else if (n / 5 > 1) {
+      a += 'V';
+      n -= 5;
+    }
+    else if (n / 4 >= 1) {
+      a += 'IV';
+      n -= 4;
+    }
+    else if (n >= 1) {
+      a += 'I';
+      n -= 1;
+    }
+  }
+  return a
 }
 
-// Drive code
+
 console.log('My totally sweet testing script\n')
 console.log('input | expected | actual')
 console.log('———|—————|———')
